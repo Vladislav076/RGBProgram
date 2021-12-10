7e87c01d4ca69d57169bf567008525d6fdc8e6fb
Vladislav Denisov <denisov076@gmail.com>
2021-12-10 20:54:55 +0300

    Initial Commit

---
+++ RGBProgram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGBProgram
+//
+//  Created by Владислав on 10.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
